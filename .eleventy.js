150f894f3548731c4738d8e881728449da971ff2
ninnlangel <info@ninnlangel.com>
2025-10-18 15:28:57 +0200

    Added markdown-it-github-callouts plugin

--- .eleventy.js
+++ .eleventy.js
@@ -1,38 +1,27 @@
-module.exports = function (eleventyConfig) {
+module.exports = async function (eleventyConfig) {
   // Copy asset files to output
   eleventyConfig.addPassthroughCopy("src/assets");
 
+  // Import ES modules dynamically
+  const markdownIt = require("markdown-it");
+  const { default: markdownItGitHubAlerts } = await import("markdown-it-github-alerts");
+
+  // Configure markdown-it with GitHub alerts plugin
+  const md = markdownIt({
+    html: true,
+    linkify: true,
+    typographer: true
+  }).use(markdownItGitHubAlerts);
+
   // Add markdown filter
   eleventyConfig.addFilter("markdown", function (content) {
-    const markdownIt = require("markdown-it")();
-    return markdownIt.render(content || "");
+    return md.render(content || "");
   });
 
   // Add inline markdown filter (no paragraph wrapping)
   eleventyConfig.addFilter("markdownInline", function (content) {
-    const markdownIt = require("markdown-it")();
-    return markdownIt.renderInline(content || "");
+    return md.renderInline(content || "");
   });
-
-  // Add category title filter
-  eleventyConfig.addFilter("categoryTitle", function (category) {
-    return category
-      .replace(/-/g, ' ')
-      .replace(/\w\S*/g, (txt) => txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase())
-      .replace(/\bcra\b/gi, 'CRA');
-  });
-
-  // Add a filter to get object keys
-  eleventyConfig.addFilter("getKeys", function(obj) {
-    return Object.keys(obj);
-  });
-
-  // Add a filter to truncate text
-  eleventyConfig.addFilter("truncate", function(text, length = 150) {
-    if (text.length <= length) return text;
-    return text.substr(0, length) + '...';
-  });
-
 
   return {
     dir: {
