da242d4e790c189eaf661dbce89daae4c9601772
Tobie Langel <tobie@unlockopen.com>
2025-10-31 00:29:52 +0100

    Allows switches branches if the FAQ cache by passing an argument to the npm scripts
    See update README for details

--- update-cache.sh
+++ update-cache.sh
@@ -2,11 +2,12 @@
 set -e
 
 REPO_URL="https://github.com/orcwg/cra-hub.git"
+BRANCH="${1:-main}"
 
 if [ -d "_cache/.git" ]; then
-  echo "🔄 Updating cache..."
-  cd _cache && git pull --rebase && cd ..
+  echo "🔄 Updating cache (branch: $BRANCH)..."
+  cd _cache && git fetch origin && git checkout $BRANCH && git pull --rebase origin $BRANCH && cd ..
 else
-  echo "📥 Cloning repo..."
-  git clone $REPO_URL _cache
+  echo "📥 Cloning repo (branch: $BRANCH)..."
+  git clone --branch $BRANCH $REPO_URL _cache
 fi
