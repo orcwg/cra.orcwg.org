300524b592c32b34742956b251c4ae36c200a419
Raphael Gregori <44970313+TiltGuy@users.noreply.github.com>
2025-11-08 22:51:25 +0100

    Update src/_data/data.js
    Co-authored-by: Tobie Langel <tobie@unlockopen.com>
    Signed-off-by: Raphael Gregori <44970313+TiltGuy@users.noreply.github.com>

--- src/_data/data.js
+++ src/_data/data.js
@@ -175,7 +175,7 @@ function getProcessedFaq(faq) {
 
   // Normalize status
   const status = faq.data.Status.replace(/^(⚠️|🛑|✅)\s*/, '').replace(" ", "-").trim().toLowerCase();
-const needsRefactoring = (/>\s*\[!WARNING\]\s*\n>\s*.*needs\s+refactoring/).test(faq.content);
+  const needsRefactoring = (/>\s*\[!WARNING\]\s*\n>\s*.*needs\s+refactoring/).test(faq.content);
   // Generate edit on github URL
   const editOnGithubUrl = new URL(`${faq.path}/${faq.filename}`, EDIT_ON_GITHUB_ROOT).href;
 
