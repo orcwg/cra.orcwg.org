35fccbace87f286b286e9ca7ff286b43c4472723
Raphaël Gregori <raphael.gregori.gd@gmail.com>
2025-11-26 11:51:46 +0100

    add detection of the alert tag

--- .eleventy.js
+++ .eleventy.js
@@ -17,7 +17,9 @@ module.exports = async function (eleventyConfig) {
     html: true,
     linkify: true,
     typographer: true
-  }).use(markdownItGitHubAlerts).use(markdownItFootnote);
+  }).use(markdownItGitHubAlerts, 
+    { markers: ["NOTE", "TIP", "IMPORTANT", "WARNING", "CAUTION", "ORC_REC"] }
+  ).use(markdownItFootnote);
 
   // Add markdown filter
   // Note: Link resolution now happens in data layer, so content already has resolved links
